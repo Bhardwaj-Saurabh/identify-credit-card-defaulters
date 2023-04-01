16f426392984732266e9f54d30d24e8f9c29b981
saurabh <aryan.saurabhbhardwaj@gmail.com>
2023-04-01 17:31:40 +0100

    created data ingestion pipeline and checked it

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.configuration.mongo_db_connection import MongoDBClient
 from src.exception import CustomException
+from src.pipeline.training_pipeline import TrainPipeline 
 import os,sys
 from src.utils.main_utils import read_yaml_file
+from src.logger import logging
 
 
 env_file_path=os.path.join(os.getcwd(),"env.yaml")
@@ -11,3 +13,17 @@ def set_env_variable(env_file_path):
     if os.getenv('MONGO_DB_URL',None) is None:
         env_config = read_yaml_file(env_file_path)
         os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
+
+
+def main():
+    try:
+        set_env_variable(env_file_path)
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
+
+
+if __name__=="__main__":
+    main()
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,16 +1,24 @@
-from src.entity.config_entity import TrainingPipelineConfig
+
+from src.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
+from src.entity.artifact_entity import DataIngestionArtifact
 from src.exception import CustomException
 import sys,os
 from src.logger import logging
+from src.components.data_ingestion import DataIngestion
 
 class TrainPipeline:
     is_pipeline_running=False
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
 
-    def start_data_ingestion(self):
+    def start_data_ingestion(self)-> DataIngestionArtifact:
         try:
-            pass
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Starting data ingestion")
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
         except  Exception as e:
             raise  CustomException(e,sys)
 
@@ -48,6 +56,6 @@ class TrainPipeline:
 
     def run_pipeline(self):
         try:
-            pass
+            data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
         except  Exception as e:
             raise  CustomException(e,sys)
